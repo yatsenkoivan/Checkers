ff4dcd07c5a991c4603b4cc88379254fb72c7fcd
Ivan Yatsenko <aokiroyal16@gmail.com>
2023-08-12 19:16:12 +0300

    Added board showing

--- Checkers.h
+++ Checkers.h
@@ -9,6 +9,11 @@ class Board {
 	private:
 		static int size_x;
 		static int size_y;
+		static int cell_size_x;
+		static int cell_size_y;
+		static int offset_y;
+		static int offset_x;
+
 		Piece*** arr;
 		int player;
 	public:
@@ -44,11 +49,33 @@ class Board {
 				}
 			}
 		}
+		void ShowCell(int row, int col) {
+			Colors fg, bg;
+			if (row % 2 == col % 2) bg = Colors::white;
+			else bg = Colors::black;
+
+			if (arr[row][col] && arr[row][col]->side == Piece::Sides::white) fg = Colors::lwhite;
+			else fg = Colors::lblack;
+
+			SetColor(fg, bg);
+
+			for (int i = 0; i < cell_size_y; i++)
+			{
+				Cursor::set(col * cell_size_x + offset_x, row*cell_size_y + i + offset_y);
+				for (int j = 0; j < cell_size_x; j++) {
+					std::cout << ' ';
+				}
+			}
+			Cursor::set(col * cell_size_x + cell_size_x / 2 + offset_x, row * cell_size_y + cell_size_y / 2 + offset_y);
+			if (arr[row][col]) std::cout << *arr[row][col];
+			std::cout << std::endl;
+			SetColor();
+			Cursor::set(0, 0);
+		}
 		void Show() {
 			for (int row = 0; row < size_y; row++) {
 				for (int col = 0; col < size_x; col++) {
-					if (arr[row][col]) std::cout << *arr[row][col];
-					else std::cout << ' ';
+					ShowCell(row, col);
 				}
 				std::cout << std::endl;
 			}
@@ -56,3 +83,7 @@ class Board {
 };
 int Board::size_x = 8;
 int Board::size_y = 8;
+int Board::cell_size_x = 5;
+int Board::cell_size_y = 3;
+int Board::offset_x = 1;
+int Board::offset_y = 2;
--- cursor.h
+++ cursor.h
@@ -3,7 +3,7 @@
 
 namespace Cursor
 {
-	void set(short x, short y) {
+	void set(SHORT x, SHORT y) {
 		HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
 		COORD pos = { x, y };
 		SetConsoleCursorPosition(h, pos);
