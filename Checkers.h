c554e37b381bc82848a0bf44bae55320384a1226
Ivan Yatsenko <aokiroyal16@gmail.com>
2023-08-12 21:31:26 +0300

    Added base movements

--- Checkers.h
+++ Checkers.h
@@ -4,6 +4,7 @@
 #include "colors.h"
 #include "Pieces.h"
 #include <conio.h>
+#include <set>
 
 
 class Board {
@@ -23,6 +24,8 @@ class Board {
 		int start_cursor_y;
 		int cursor_x;
 		int cursor_y;
+
+		std::set<std::pair<int, int>> move_variants;
 
 	public:
 		Board()
@@ -85,6 +88,93 @@ class Board {
 		{
 			ShowCell(cursor_y, cursor_x);
 		}
+		void CancelMove()
+		{
+			//CELL WAS NOT CHOOSED
+			if (start_cursor_x == -1 && start_cursor_y == -1) return;
+			int x = start_cursor_x;
+			int y = start_cursor_y;
+			start_cursor_x = -1;
+			start_cursor_y = -1;
+			ShowCell(y, x);
+			DelMoveVariants();
+		}
+		void MakeAMove()
+		{
+			//START CELL
+			if (start_cursor_x == -1 && start_cursor_y == -1)
+			{
+				//STARTING WITH AN EMPTY CELL
+				if (arr[cursor_y][cursor_x] == nullptr) return;
+
+				//STARTING WITH AN ENEMY PIECE
+				if ((arr[cursor_y][cursor_x]->side == Piece::Sides::black && player == 1)
+					||
+					(arr[cursor_y][cursor_x]->side == Piece::Sides::white && player == 2)) return;
+
+				start_cursor_x = cursor_x;
+				start_cursor_y = cursor_y;
+				SetMoveVariants(cursor_x, cursor_y);
+				ShowMoveVariants();
+			}
+
+			//END CELL
+			else
+			{
+				//CORRECT MOVE
+				if (move_variants.find(std::pair<int, int>(cursor_x, cursor_y)) != move_variants.end())
+				{
+					arr[cursor_y][cursor_x] = arr[start_cursor_y][start_cursor_x];
+					arr[start_cursor_y][start_cursor_x] = nullptr;
+					ShowCell(cursor_y, cursor_x);
+					CancelMove();
+					player = (player == 1 ? 2 : 1);
+					ShowPlayer();
+				}
+			}
+		}
+		void ShowMoveVariants()
+		{
+			for (std::pair<int, int> coords : move_variants)
+			{
+				ShowCell(coords.second, coords.first);
+			}
+		}
+		void DelMoveVariants()
+		{
+			std::set<std::pair<int, int>> move_variants_copy = move_variants;
+			move_variants.clear();
+			for (std::pair<int, int> coords : move_variants_copy)
+			{
+				ShowCell(coords.second, coords.first);
+			}
+		}
+		void SetMoveVariants(int x, int y)
+		{
+			move_variants.clear();
+			if ((x >= 0 || x <= size_x - 1) == false) return;
+			if ((y >= 0 || y <= size_y - 1) == false) return;
+			if (arr[y][x] == nullptr) return;
+			
+			//white (or king)
+			if (arr[y][x]->side == Piece::Sides::white || arr[y][x]->type == Piece::Types::king)
+			{
+				//up 1 . left 1 (empty)
+				if (x - 1 >= 0 && y - 1 >= 0 && arr[y-1][x-1] == nullptr) move_variants.insert(std::pair<int, int>(x - 1, y - 1));
+				//up 1 . right 1 (empty)
+				if (x + 1 <= size_x-1 && y - 1 >= 0 && arr[y-1][x+1] == nullptr) move_variants.insert(std::pair<int, int>(x + 1, y - 1));
+			}
+
+			//black (or king)
+			if (arr[y][x]->side == Piece::Sides::black || arr[y][x]->type == Piece::Types::king)
+			{
+				//down 1 . left 1 (empty)
+				if (x - 1 >= 0 && y + 1 <= size_y-1 && arr[y + 1][x - 1] == nullptr) move_variants.insert(std::pair<int, int>(x - 1, y + 1));
+				//up 1 . right 1 (empty)
+				if (x + 1 <= size_x - 1 && y + 1 <= size_y-1 && arr[y + 1][x + 1] == nullptr) move_variants.insert(std::pair<int, int>(x + 1, y + 1));
+			}
+
+		}
 		void Press(char key)
 		{
 			HideCursor();
@@ -118,6 +208,12 @@ class Board {
 				//MAKE-A-MOVE
 				case 'f':
 				case 'F':
+					MakeAMove();
+					break;
+				//CANCEL-A-MOVE
+				case 'x':
+				case 'X':
+					CancelMove();
 					break;
 			}
 			ShowCursor();
@@ -157,10 +253,15 @@ class Board {
 		void ShowCell(int row, int col, bool ShowCursor = true)
 		{
 			bool IsCursor = false;
+			bool IsVariant = false;
 
 			if ((ShowCursor && row == cursor_y && col == cursor_x)
 				||
 				(row == start_cursor_y && col == start_cursor_x)) IsCursor = true;
+
+			//IS VARIANT
+			if (move_variants.find(std::pair<int, int>(col, row)) != move_variants.end())
+				IsVariant = true;
 
 			Colors fg, bg;
 			if (row % 2 == col % 2) bg = Colors::white;
@@ -174,6 +275,8 @@ class Board {
 			if (IsCursor)
 				SetColor(Colors::cursor, bg);
 
+			if (IsVariant)
+				SetColor(Colors::variant_fg, Colors::variant_bg);
 
 			for (int i = 0; i < cell_size_y; i++)
 			{
